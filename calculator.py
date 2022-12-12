811f3101dee5b3305efc604f52cd8c4b8548f7d3
Imad Ibrahim <imadibrahim164@gmail.com>
2022-12-12 14:01:12 +0530

    Added padding to text (pady=10)

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 window = tk.Tk()
 window.geometry("350x500")
-window = tk.Text(window, height=1, width=30)
+wind = tk.Text(window, height=1, width=30, font="size=60")
 
-window.pack()
+wind.pack(pady=10)
 window.mainloop()
