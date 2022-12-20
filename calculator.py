9a9f04a97a4a3ebb5550e2086e713513c1e9209b
Imad Ibrahim <imadibrahim164@gmail.com>
2022-12-20 17:50:51 +0530

    Added grid and also added back button

--- calculator.py
+++ calculator.py
@@ -25,30 +25,66 @@ def clear():
     text_values.set("")
 
 
-but_frame = Frame(window, width=312, height=272.5, bg="grey")
-#
-Bt1 = Button(window, text=" 1 ", command=lambda: on_click("1"))
-Bt2 = Button(window, text=" 2 ", command=lambda: on_click("2"))
-Bt3 = Button(window, text=" 3 ", command=lambda: on_click("3"))
-Bt4 = Button(window, text=" 4 ", command=lambda: on_click("4"))
-Bt5 = Button(window, text=" 5 ", command=lambda: on_click("5"))
-Bt6 = Button(window, text=" 6 ", command=lambda: on_click("6"))
-Bt7 = Button(window, text=" 7 ", command=lambda: on_click("7"))
-Bt8 = Button(window, text=" 8 ", command=lambda: on_click("8"))
-Bt9 = Button(window, text=" 9 ", command=lambda: on_click("9"))
-Bt0 = Button(window, text=" 0 ", command=lambda: on_click("0"))
+def equalto():
+    global expression
+    result = eval(expression)
+    text_values.set(result)
 
-Btdiv = Button(window, text=" / ", command=lambda: on_click("/"))
-Btadd = Button(window, text=" + ", command=lambda: on_click("+"))
-Btminus = Button(window, text=" - ", command=lambda: on_click("-"))
 
-Btc = Button(window, text=" C ", command=lambda: clear())
+def back():
+    global expression
+    back_value = expression[:-1]
+    expression = back_value
+    text_values.set(expression)
 
-#
-text_display.grid(row=0, column=0, columnspan=20, padx=10, pady=10)
-Bt1.grid(row=1, column=0, padx=10, pady=10)
-Bt2.grid(row=2, column=0, padx=10, pady=10)
-Btm.grid(row=2, column=1, padx=10, pady=10)
-Btc.grid(row=2, column=2, padx=10, pady=10)
+
+Bt1 = Button(window, text=" 1 ", command=lambda: on_click("1"), width=7, height=2)
+Bt2 = Button(window, text=" 2 ", command=lambda: on_click("2"), width=7, height=2)
+Bt3 = Button(window, text=" 3 ", command=lambda: on_click("3"), width=7, height=2)
+Bt4 = Button(window, text=" 4 ", command=lambda: on_click("4"), width=7, height=2)
+Bt5 = Button(window, text=" 5 ", command=lambda: on_click("5"), width=7, height=2)
+Bt6 = Button(window, text=" 6 ", command=lambda: on_click("6"), width=7, height=2)
+Bt7 = Button(window, text=" 7 ", command=lambda: on_click("7"), width=7, height=2)
+Bt8 = Button(window, text=" 8 ", command=lambda: on_click("8"), width=7, height=2)
+Bt9 = Button(window, text=" 9 ", command=lambda: on_click("9"), width=7, height=2)
+Bt0 = Button(window, text=" 0 ", command=lambda: on_click("0"), width=7, height=2)
+Btd = Button(window, text=" . ", command=lambda: on_click("."), width=7, height=2)
+Btdel = Button(window, text=" < ", command=lambda: back(), width=16, height=2)
+
+
+Btdiv = Button(window, text=" / ", command=lambda: on_click("/"), width=7, height=2)
+Btadd = Button(window, text=" + ", command=lambda: on_click("+"), width=7, height=2)
+Btminus = Button(window, text=" - ", command=lambda: on_click("-"), width=7, height=2)
+Btx = Button(window, text=" * ", command=lambda: on_click("*"), width=7, height=2)
+
+
+Btq = Button(window, text=" = ", command=lambda: equalto(), width=16, height=2)
+Btc = Button(window, text=" C ", command=lambda: clear(), width=16, height=2)
+
+
+text_display.grid(row=0, column=0, columnspan=20, padx=10, pady=20)
+
+Bt7.grid(row=2, column=1, pady=3)
+Bt8.grid(row=2, column=2)
+Bt9.grid(row=2, column=3)
+
+Bt4.grid(row=3, column=1, pady=3)
+Bt5.grid(row=3, column=2)
+Bt6.grid(row=3, column=3)
+
+Bt1.grid(row=4, column=1, pady=3)
+Bt2.grid(row=4, column=2)
+Bt3.grid(row=4, column=3)
+
+Btd.grid(row=5, column=1)
+Bt0.grid(row=5, column=2)
+Btq.grid(row=5, column=3, columnspan=2)
+
+Btadd.grid(row=4, column=4)
+Btminus.grid(row=3, column=4)
+Btx.grid(row=2, column=4)
+
+Btc.grid(row=1, column=1, columnspan=2)
+Btdel.grid(row=1, column=3, columnspan=2)
 
 window.mainloop()
